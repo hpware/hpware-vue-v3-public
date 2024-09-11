6948b829f5e80ca3bf46301f4faab297a4cb267c
吳元皓 <hpware3@gmail.com>
2024-09-11 23:06:29 +0800

    Test folder, this will be pushed to prod soon I guess.

---
+++ src/Pages/Test/Contact.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <h2>連絡</h2>
+  <form>
+    <input type="text" placeholder="姓名"><br>
+    <input type="email" placeholder="電子郵件"><br>
+    <textarea placeholder="訊息"></textarea><br>
+    <button @onclick="">送出</button>
+  </form>
+</template>
+
+<style scoped>
+input{
+  width: 60%;
+  margin-bottom: 1em;
+  background-color: burlywood;
+}
+</style>
