d297e206ae892a10b9ea4f3a57977b544ec20401
吳元皓 <hpware3@gmail.com>
2024-09-13 00:22:06 +0800

    Miscellaneous pages.

--- src/Pages/About.vue
+++ src/Pages/About.vue
@@ -3,14 +3,14 @@
 <template>
   <h2>About Me</h2>
   <section id="aboutme">
-    <h1>自我介紹</h1>
+    <h4>自我介紹</h4>
     <p>我是一個學生，準備在臺灣就讀有關資訊工程的五專。</p>
     <p>熟悉 HTML, CSS , Vercel 與 Git(Hub) </p>
   </section>
   <br>
   <hr>
   <section id="certifications">
-    <h1>檢定通過</h1>
+    <h4>檢定通過</h4>
     <p style="font-size: 1.5em; padding-bottom: 0;">GEPT</p>
     <span>初級</span><br>
     <span>中級聽讀</span>
