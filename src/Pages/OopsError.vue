b97d2aed5b326c5aa8ffbce506d9c953c1ddc2e8
吳元皓 <157942818+hpware@users.noreply.github.com>
2024-09-13 12:43:21 +0800

    Update OopsError.vue

--- src/Pages/OopsError.vue
+++ src/Pages/OopsError.vue
@@ -3,12 +3,19 @@
 
 <template>
   <h1>404</h1>
-  <p>穿梭者，你好<br>
+  <p lang="zh-tw">穿梭者，你好<br>
     你已經踏入到一個所人未知的地方<br>
     你可能需要幫忙一下<br>
     <a href="/">這裡</a>應該可以幫你導覽您回去正確的道路<br>
     如果真的回不去請寄信件給我： <a href="mailto:web+errorpage@yuanhau.com">web@yuanhau.com</a>
   </p>
+    <p lang="en">Hello, Traveler<br>
+    You have entered into a black hole.<br>
+   You might need some help<br>
+    <a href="/">This page</a> may help you go back to the right place.<br>
+    If this is my error You can email me: <a href="mailto:web+errorpage@yuanhau.com">here</a>
+  </p>
+  
 </template>
 
 <style scoped>
