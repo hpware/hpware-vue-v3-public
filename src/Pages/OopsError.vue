7595311699ed5bdd7d5979c4430ba1be61770d4c
吳元皓 <157942818+hpware@users.noreply.github.com>
2024-09-13 12:34:53 +0800

    Update OopsError.vue

--- src/Pages/OopsError.vue
+++ src/Pages/OopsError.vue
@@ -3,10 +3,11 @@
 
 <template>
   <h1>404</h1>
-  <p>網頁穿梭者，你好<br>
+  <p>穿梭者，你好<br>
     你已經踏入到一個所人未知的地方<br>
     你可能需要幫忙一下<br>
-    <a href="/">這裡</a>可以幫你回去正確的道路
+    <a href="/">這裡</a>應該可以幫你導覽您回去正確的道路<br>
+    如果真的回不去請寄信件給我： <a href="mailto:web+errorpage@yuanhau.com">web+errorpage@yuanhau.com</a>
   </p>
 </template>
 
@@ -27,10 +28,6 @@ h1 {
 p {
   font-size: 1.5em;
   margin-bottom: 0;
-}
-
-a {
-  color: white;
 }
 
 a:hover {
