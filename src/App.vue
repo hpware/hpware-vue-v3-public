471c3146ed6d965d95b299a026b36f4305e06e9c
Howard <hw@hwtw.cc>
2024-09-13 23:00:50 +0800

    Add Mobile Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <script setup>
-import NavSpace from '@/components/NavSpace.vue'
+import {ref} from 'vue';
+const navmobile = ref(false);
+
 </script>
 <template>
   <nav>
     <h1>吳元皓</h1>
-    <RouterLink to="/">首頁</RouterLink><NavSpace/>
-    <a href="/Go?l=blog">Blog</a><NavSpace/>
-    <RouterLink to="/About">關於我</RouterLink><NavSpace/>
-    <RouterLink to="/Projects">專案</RouterLink><span class="mobile">&nbsp;</span>
+    <div class="nav-mobile">
+    <button @click="navmobile = !navmobile;" class="not-a-button">選單</button>
+      <Transition name="fade">
+      <div v-if="navmobile" style="  color: rgba(191, 191, 191, 0.7);">
+        <RouterLink to="/">首頁</RouterLink><br>
+        <RouterLink to="/Go?l=blog">Blog</RouterLink><br>
+        <RouterLink to="/About">關於我</RouterLink><br>
+        <RouterLink to="/Projects">專案</RouterLink>
+      </div>
+    </Transition>
+    </div>
+    <div class="nav-desktop">
+    <RouterLink to="/">首頁</RouterLink>&nbsp;
+    <RouterLink to="/Go?l=blog">Blog</RouterLink>&nbsp;
+    <RouterLink to="/About">關於我</RouterLink>&nbsp;
+    <RouterLink to="/Projects">專案</RouterLink>
+    </div>
   </nav>
   <main>
     <RouterView />
@@ -27,6 +42,29 @@ import NavSpace from '@/components/NavSpace.vue'
 footer {
   a{
     color:aqua
+  }
+}
+div.nav-mobile {
+  display: none;
+}
+.not-a-button {
+  background-color: transparent;
+  border: none;
+  color: rgba(191, 191, 191, 0.7);
+  font-size: 1.0em;
+  cursor: pointer;
+  border-radius: 50%;
+}
+
+div.nav-desktop {
+  display: block;
+}
+@media (max-width: 600px) {
+  div.nav-mobile {
+    display: block;
+  }
+  div.nav-desktop {
+    display: none;
   }
 }
 </style>
