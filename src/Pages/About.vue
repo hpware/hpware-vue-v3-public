1d80458590ca19ccd951705c35f0a834ebffd093
Howard <hw@hwtw.cc>
2024-09-14 19:03:35 +0800

    增加自我介紹頁面

--- src/Pages/About.vue
+++ src/Pages/About.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 </script>
 <template>
-  <h2>About Me</h2>
+  <h3>關於我</h3>
   <section id="aboutme">
     <h4>自我介紹</h4>
     <p>我是一個學生，準備在臺灣就讀有關資訊工程的五專。</p>
     <p>熟悉 HTML, CSS , Vercel 與 Git(Hub) </p>
     <p>我喜歡把 Prod 當測試環境。</p>
     <p>小時候是我就喜歡用電腦，但我當時一點程式都不會寫。<br>
-      我剛開始想要並使用自己的Wordpress的部落格<a href="https://hwweb.i234.me/wordpress/">https://hwweb.i234.me/wordpress</a>，<br>但Synology 不讓我把Wordpress放在 Website Root 裡面，所以我決定要改預設的檔案來讓使用者到部落格。</p>
+      我剛開始想要並使用自己的Wordpress的部落格<a href="https://hwweb.i234.me/wordpress/">https://hwweb.i234.me/wordpress</a>，
+      <br>但Synology 不讓我把Wordpress放在 Website Root 裡面，所以我決定要改預設的檔案來讓使用者到部落格。
+    </p>
   </section>
   <br>
   <hr>
