2f0e5e8b1f0bf2b453c29ad2e036ed211e4a46c4
hpware-bot1 <bot+announcements@yuanhau.com>
2024-09-14 16:26:59 +0800

    更新公告，改成 此網站已增加快速公告更改系統 Powered by n8n!

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="a">
-    <p><i class="bi bi-megaphone-fill"></i>&nbsp;此網站已增加錯誤追蹤系統！</p>
+    <p><i class="bi bi-megaphone-fill"></i>&nbsp;此網站已增加快速公告更改系統 Powered by n8n!</p>
   </div>
 </template>
 
