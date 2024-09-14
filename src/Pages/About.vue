410542126e4166ad75abbc0233ccaec981f369c3
吳元皓 <hpware3@gmail.com>
2024-09-14 21:34:58 +0800

    Add more things

--- src/Pages/About.vue
+++ src/Pages/About.vue
@@ -8,11 +8,18 @@
     <p>熟悉 HTML, CSS , Vercel 與 Git(Hub) </p>
     <p>我喜歡把 Prod 當測試環境。</p>
     <p>小時候是我就喜歡用電腦，但我當時一點程式都不會寫。<br>
-      我剛開始想要並使用自己的Wordpress的部落格<a href="https://hwweb.i234.me/wordpress">hwweb.i234.me/wordpress</a>，
+      我2020年時剛開始想要並使用自己的Wordpress的部落格<a href="https://hwweb.i234.me/wordpress">hwweb.i234.me/wordpress</a>，
       <br>但Synology 不讓我把Wordpress放在 Website Root 裡面，所以我決定要改預設的檔案來讓使用者到部落格。
       <br>在Github上發現在我的舊帳號上有一個備份<a href="https://github.com/thehwtw/wncx">github.com/thehwtw/wncx</a>
       <br>當時的我沒有放入< a > 的標籤，所以不是 HyperLink，我當時也沒想太多就繼續使用著。
       <br>但我在這一年內增加了許多HTML & CSS的知識，讓我更有信心可以做的更好！
+    </p>
+    <p>Ai 來襲時我對 AI 有些好感，例如：他可以幫我做 New & Improved 的 部落格導向
+    <br>就是現在的 location.href 就是 AI 教我的，我認為這個是一個簡單又easy的方法。
+    </p>
+    <p>
+      我也在國中一年級時開始學習Python，這個也是我想學的語言，但我教學影片只看但10％的地方就放棄了。
+      <br>我也之後大多數學的都忘記了，if else 每次都要查一下，因為我比較習慣用JavaScript 的方式 if {}。
     </p>
   </section>
   <br>
