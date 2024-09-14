848cebd107a1fec2788bc42fee5826261498d58d
Howard <hw@hwtw.cc>
2024-09-14 11:00:30 +0800

    Add Sentry!

--- src/sentry.js
+++ src/sentry.js
@@ -1,18 +1,14 @@
-import '@/assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
 import router from './router'
 import * as Sentry from "@sentry/vue";
 
-const app = createApp(App)
-
 Sentry.init({
     app,
-    dsn: "https://76de269598297639e16a87e22e404f5d@o4507948895174656.ingest.us.sentry.io/4507948953698304",
+    dsn: "https://6092c0bbe9603caeeaff9c3218a53ae9@o4507948895174656.ingest.us.sentry.io/4507948897992704",
     integrations: [
         Sentry.browserTracingIntegration({ router }),
         Sentry.replayIntegration(),
+        Sentry.browserProfilingIntegration(),
     ],
     // Tracing
     tracesSampleRate: 1.0, //  Capture 100% of the transactions
@@ -21,7 +17,11 @@ Sentry.init({
     // Session Replay
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    // Profiling
+    profilesSampleRate: 1.0, // Profile 100% of the transactions. This value is relative to tracesSampleRate
 });
 
-app.use(router)
-app.mount('#app')
+new Vue({
+    router,
+    render: (h) => h(App),
+}).$mount("#app");
