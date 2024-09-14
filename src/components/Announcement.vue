ff25bbe299ec8d522ed240be24f5a577e94195e9
hpware-bot1 <bot+announcements@yuanhau.com>
2024-09-14 16:24:53 +0800

    更新公告，改成 此網站已增加錯誤追蹤系統！

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="a">
-    <p><i class="bi bi-megaphone-fill"></i>&nbsp;{{ $('If').item.json['公告訊息'] }}</p>
+    <p><i class="bi bi-megaphone-fill"></i>&nbsp;此網站已增加錯誤追蹤系統！</p>
   </div>
 </template>
 
