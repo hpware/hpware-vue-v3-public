bb6e8b21862b970ba89bab048fa6e15eaac0ea16
hpware-bot1 <bot+announcements@yuanhau.com>
2024-09-14 17:05:48 +0800

    更新公告，改成 此網站還在開發中！Github 是 hpware/hpware-vue-v3

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="a">
-    <p><i class="bi bi-megaphone-fill"></i>&nbsp;</p><script>alert(1)</script><p></p>
+    <p><i class="bi bi-megaphone-fill"></i>&nbsp;此網站還在開發中！Github 是 hpware/hpware-vue-v3</p>
   </div>
 </template>
 
