37914678c4a429df24b0dea747b297c606ac686e
hpware-bot1 <bot+announcements@yuanhau.com>
2024-09-14 17:12:29 +0800

    公告關閉

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -4,7 +4,6 @@
 
 <template>
   <div class="a">
-    <p><i class="bi bi-megaphone-fill"></i>&nbsp;此網站還在開發中！Github 是 hpware/hpware-vue-v3</p>
   </div>
 </template>
 
@@ -12,6 +11,9 @@
 
 .a {
   font-size: 0.6em;
+  color: yellow;
+}
+a {
   color: yellow;
 }
 </style>
