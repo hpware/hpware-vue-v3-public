8da15f69e44efbac6f8a97caab3a10dc520755e8
Howard <hw@hwtw.cc>
2024-09-15 15:36:30 +0800

    Add more Projects

--- src/Pages/About/AboutThisProject.vue
+++ src/Pages/About/AboutThisProject.vue
@@ -4,7 +4,8 @@ import SiteV3 from '@/components/Projects/site-v3.vue';
 import Lovemusicasiasite from '@/components/Projects/lovemusicasiasite.vue';
 import AdGuardBlockList from '@/components/Projects/AdGuardBlockList.vue';
 import hwtw_cc from '@/components/Projects/hwtw-cc.vue';
-// Import OopsError
+import LinuxInstallScript from '@/components/Projects/Linux安裝指令.vue';
+// Import OopsError + CSS
 import OopsError from '@/Pages/OopsError.vue';
 // Pull URL variables and set to all lowercase
 const page_content_URL = new URLSearchParams(location.search).get("page");
@@ -12,7 +13,8 @@ let page_content = `${page_content_URL.toLowerCase()}`;
 </script>
 
 <template>
-  <div v-if="page_content === 'lovemusicasiasite'">
+  <div class="aboutthisproject">
+  <div v-if="page_content === 'lovemusicasia'">
     <Lovemusicasiasite />
   </div>
   <div v-else-if="page_content === 'site-v3'">
@@ -24,17 +26,23 @@ let page_content = `${page_content_URL.toLowerCase()}`;
   <div v-else-if="page_content === 'hwtw-cc'">
     <hwtw_cc />
   </div>
+    <div v-else-if="page_content === 'linux安裝指令' ">
+      <LinuxInstallScript />
+    </div>
   <div v-else>
     <OopsError />
+  </div>
   </div>
 </template>
 
 <style scoped>
-p.text-error {
+div.aboutthisproject {
+  p.text-error {
     font-size: 1.5em;
     margin-bottom: 0;
-}
-body {
-  font-size: 1.0em;
+  }
+  body {
+    font-size: 1.0em;
+  }
 }
 </style>
---
+++ src/components/Projects/Linux安裝指令.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+<h3>Linux 安裝指令</h3>
+  <p>
+    Ubuntu 24.04 + Gnome<br>
+    curl https://raw.githubusercontent.com/hpware/hpware/main/install/ubuntu-gnome.sh | bash<br>
+
+    Non Gnome Ubuntu:<br>
+    curl -fsSL https://raw.githubusercontent.com/hpware/hpware/main/install/ubuntu.sh | bash
+<br>
+    Debian 12 + Gnome
+    <br>curl -fsSL https://raw.githubusercontent.com/hpware/hpware/main/install/debian-gnome.sh | bash
+
+    <br>Non Gnome Debian :
+    <br>curl -fsSL https://raw.githubusercontent.com/hpware/hpware/main/install/debian.sh | bash
+
+    <br>Backup PC:
+    <br>curl -fsSL https://raw.githubusercontent.com/hpware/hpware/main/install/backup.sh | bash
+  </p>
+  <br>
+  <a href="https://old.hwtw.cc/script/linux-install.txt"><button>安裝指令 TXT</button></a>&nbsp;<a href="https://github.com/hpware/hpware/blob/main/install/"><button>安裝指令 sh 檔案</button></a>
+</template>
+
+<style scoped>
+
+</style>
