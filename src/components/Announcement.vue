aafd8eb528692958f73b70ac8fe221c12488afb8
hpware-bot1 <bot+announcements@yuanhau.com>
2024-09-15 18:08:44 +0800

    更新公告，改成 新 Threads! <a href=“ https://www.threads.net/@yh_.5_26/post/C_7gokZPNnW/?xmt=AQGzJC3jYSY4ykdGBV6jXoDz18qpzU-Xt3TBtNEntg_1UQ”>貼文</a>

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="a">
-    <p><i class="bi bi-megaphone-fill"></i>&nbsp;CSS 已正常！</p>
+    <p><i class="bi bi-megaphone-fill"></i>&nbsp;新 Threads! <a href=“ https://www.threads.net/@yh_.5_26/post/C_7gokZPNnW/?xmt=AQGzJC3jYSY4ykdGBV6jXoDz18qpzU-Xt3TBtNEntg_1UQ”>貼文</a></p>
   </div>
 </template>
 
