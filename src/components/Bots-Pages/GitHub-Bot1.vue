241e28f4319da0a787671470109282c6249d0be0
吳元皓 <hpware3@gmail.com>
2024-09-15 22:08:03 +0800

    增加資訊

--- src/components/Bots-Pages/GitHub-Bot1.vue
+++ src/components/Bots-Pages/GitHub-Bot1.vue
@@ -5,6 +5,7 @@
 <template>
   <h3>GitHub 機器人1</h3>
   <p>這個機器人是 powered by n8n </p>
+  <p>創做主要的原因：idk</p>
   <br>
   <a href="https://github.com/hpware-bot1/"><button>GitHub Bot (遭移除)</button></a>
 </template>
