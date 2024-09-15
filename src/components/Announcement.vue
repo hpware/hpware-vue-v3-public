95c06f979da56bcb5ed11f279469a531d89654e1
hpware-bot1 <bot+announcements@yuanhau.com>
2024-09-15 18:26:28 +0800

    更新公告，改成 <a href=“https://go64.cc/u9z4IW”>新 Thread !</a>

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="a">
-    <p><i class="bi bi-megaphone-fill"></i>&nbsp;新 Threads! <a href=“ https://www.threads.net/@yh_.5_26/post/C_7gokZPNnW/?xmt=AQGzJC3jYSY4ykdGBV6jXoDz18qpzU-Xt3TBtNEntg_1UQ”>貼文</a></p>
+    <p><i class="bi bi-megaphone-fill"></i>&nbsp;<a href=“https://go64.cc/u9z4IW”>新 Thread !</a></p>
   </div>
 </template>
 
