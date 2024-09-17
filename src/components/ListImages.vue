5d987a3439a3725c07d5e705e84abd78edf6e49b
吳元皓 <hpware3@gmail.com>
2024-09-17 11:28:22 +0800

    Add broken ListImages!

---
+++ src/components/ListImages.vue
@@ -0,0 +1,19 @@
+<script setup>
+export default {
+  props: {
+    file: {
+      type: String,
+      required: true
+    }
+  }
+}
+
+</script>
+
+<template>
+  <a href={{file}}><img src={{file}} alt={{file}}></a>
+</template>
+
+<style scoped>
+
+</style>
