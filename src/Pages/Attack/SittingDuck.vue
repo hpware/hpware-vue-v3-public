c524c722835e5f83ff82db676860921853af1603
吳元皓 <hpware3@gmail.com>
2024-09-17 11:51:28 +0800

    The sitting duck attack thing.

---
+++ src/Pages/Attack/SittingDuck.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <img src="https://zz.yuanhau.com/u/V4KPEW.png" />
+  <h3>
+    您嘗試造訪的網站遭受了Sitting Duck的攻擊
+  </h3>
+  <p>您可以用以下的連結了解更多相關資訊<br>
+    <a href="https://krebsonsecurity.com/2024/07/dont-let-your-domain-name-become-a-sitting-duck/">更多資訊</a></p>
+</template>
+
+<style>
+img {
+  width:90%;
+}
+</style>
