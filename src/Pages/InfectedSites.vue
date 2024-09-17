f618e07e58735310ab5a66e221dad0b65a410acf
吳元皓 <hpware3@gmail.com>
2024-09-17 16:23:38 +0800

    Add InfectedSites

---
+++ src/Pages/InfectedSites.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ist from '@/components/InfectedSitesTemplate.vue';
+</script>
+
+<template>
+  <h2>已中毒網站與網頁</h2>
+  <ist
+       name="gh.yuanh.xyz"
+       infectReason="因為 GitHub Pages 的系統，這個網域變成一個Sitting Duck。"
+       afterMath="Google Search 已除項目，網域也把CNAME移除。"
+       foundDate="2024-09-14"
+       infectReasonURL="https://krebsonsecurity.com/2024/07/dont-let-your-domain-name-become-a-sitting-duck/"
+       fix="true"></ist>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/InfectedSitesTemplate.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  infectReasonURL: {
+    type: String,
+    required: true
+  },
+  infectReason: {
+    type: String,
+    required: true
+  },
+  website: {
+    type: String,
+    required: false
+  },
+  moreText: {
+    type: String,
+    required: false
+  },
+  fix: {
+    type: String,
+    required: true
+  },
+  foundDate: {
+    type: String,
+    required: true
+  },
+  afterMath: {
+    type: String,
+    required: false
+  }
+});
+</script>
+
+<template>
+  <section :id="props.name">
+  <h4>{{props.name}}&nbsp;<span v-if="props.fix === 'true'">(已修復)</span>
+    <span v-if="props.fix === 'false'">(未修復)</span></h4>
+  <p>發現日期: {{props.foundDate}}</p>
+  <p>中毒原因: {{props.infectReason}}</p>
+  <p>解決：{{props.afterMath}}
+    <br>
+
+    <span v-if="props.moreText" v-html="props.moreText"></span><br v-if="props.moreText">
+    <a :href="props.infectReasonURL">更多資訊</a>
+  </p>
+  </section>
+</template>
+
+<style scoped>
+p {
+  margin-top: 0.4em;
+  margin-bottom: 0.4em;
+}
+</style>
