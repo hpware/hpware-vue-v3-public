7b4cea83e49e0791997bd9c0a6b8cfe80430e60a
Howard <hw@hwtw.cc>
2024-09-18 22:45:39 +0800

    Make the admin page function better!

---
+++ src/Pages/Admin.vue
@@ -0,0 +1,62 @@
+<script setup>
+// Default
+import { ref } from 'vue';
+// Login Function
+const login = ref('no');
+const pwd = ref('');
+const ENVpwd = import.meta.env.VITE_ADMINPWD;
+const checkPassword = () => {
+  if (pwd.value === ENVpwd) {
+    login.value = 'yes';
+  } else {
+    login.value = 'failed';
+  }
+}
+// Announcements
+import Announcements from '@/components/Admin/Announcements.vue'
+const AnnouncementsTransition = ref(false);
+const toggleAnnouncements = () => {
+  AnnouncementsTransition.value = !AnnouncementsTransition.value;
+}
+</script>
+
+<template>
+  <div class="admin-page">
+    <div v-if="login === 'yes'">
+      <h3>Admin Page</h3>
+      <p>Panels:</p><br>
+      <button @click="toggleAnnouncements();">公告更改</button>
+      <Transition name="fade">
+        <div v-if="AnnouncementsTransition">
+          <Announcements />
+        </div>
+      </Transition>
+    </div>
+    <div v-if="login === 'no'">
+      <h3>Admin Login Page</h3>
+      <p>You are trying to access a password protected area.</p><br>
+      <form @submit="checkPassword">
+        <label for="password">Password</label><br>
+        <input type="password" v-model="pwd" required>
+        <br>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    <div v-if="login === 'failed'">
+      <h3>Admin Page</h3>
+      <p>Failed to login</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+input, textarea {
+  border-radius: 5px;
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid #ccc;
+}
+textarea {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
--- src/Pages/Admin/Index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="admin-page">
-    <h3>Admin Page</h3>
-    <p>Links:</p>
-    <RouterLink to="/Admin/Announcements">公告更改</RouterLink><br>
-  </div>
-</template>
-<script setup>
-</script>
