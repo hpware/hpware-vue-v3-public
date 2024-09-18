23bafbe4016f9b47ccc06ca6fcb61edde056363b
Howard <hw@hwtw.cc>
2024-09-18 23:55:33 +0800

    Add GitHub Orgs to the abouts

---
+++ src/Pages/About/GitHub-Org.vue
@@ -0,0 +1,27 @@
+<script setup>
+// Pull URL variables and set to all lowercase
+const page_content_URL = new URLSearchParams(location.search).get("page");
+let page_content = `${page_content_URL.toLowerCase()}`;
+// Import Error page and Vue Ref & defineAsyncComponent
+import OopsError from '@/Pages/OopsError.vue';
+import { ref, defineAsyncComponent } from 'vue';
+// Import Web pages.
+const pages = {
+  'lmsite-tw': () => import('@/components/GitHub-Org/lmsite-tw.vue'),
+};
+const component = ref(null);
+if (pages[page_content]) {
+  component.value = defineAsyncComponent(pages[page_content]);
+  var displaytitle = ref(true);
+} else {
+  component.value = OopsError;
+  var displaytitle = ref(false);
+
+}
+</script>
+<template>
+  <h2 v-if="displaytitle = true">關於這個GitHub組織</h2>
+  <div class="aboutthisproject" id="project">
+    <component :is="component"></component>
+  </div>
+</template>
---
+++ src/components/GitHub-Orgs/lmsite-tw.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+<h3>lmsite-tw</h3>
+  <h4>網頁未完成!</h4>
+</template>
+
+<style scoped>
+
+</style>
