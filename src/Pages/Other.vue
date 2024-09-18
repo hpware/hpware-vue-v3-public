5337fed1c41c9f6eed30f9c7c73dd1a00679cfeb
Howard <hw@hwtw.cc>
2024-09-18 11:16:22 +0800

    Fix some bugs in the website.

--- src/Pages/Other.vue
+++ src/Pages/Other.vue
@@ -1,12 +1,16 @@
 <script setup>
 
+import NavSpace from "@/components/NavSpace.vue";
 </script>
 
 <template>
   <h2>其他</h2>
   <p>
     <a href="/InfectedSites">已中毒網站與網頁</a><br>
-    <a href="https://automation.yuanh.xyz/form/公告訊息">改變 Announcement</a>
+    <a href="https://automation.yuanh.xyz/form/公告訊息">改變 Announcement</a><br>
+    <a href="https://git.hwtw.cc">Git 伺服器 2</a><br>
+    <a href="https://zz.yuanhau.com">檔案分享伺服器</a><br>
+      <a href="https://vd.yuanhau.com">使用者調查系統</a><br>
   </p>
 </template>
 
--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="a">
-    <p><i class="bi bi-megaphone-fill"></i>&nbsp;Oracle, it’s time to free JavaScript. <a href="https://javascript.tm">Sign this proposal</a>!</p>
+    <p><i class="bi bi-megaphone-fill"></i>&nbsp;Oracle, it's time to free JavaScript. <a href="https://javascript.tm">Sign this proposal</a>!</p>
   </div>
 </template>
 
