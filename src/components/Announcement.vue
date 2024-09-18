e50955b97aff3d4989b7a86dd942e1d8406a4352
hpware-bot1 <bot+announcements@yuanhau.com>
2024-09-18 22:48:29 +0800

    更新公告，改成 <a href="https://javascript.tm/">Tell Oracle to free Javascript</a>

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="a">
-    <p><i class="bi bi-megaphone-fill"></i>&nbsp;Tell Oracle to free Javascript <a href="https://javascript.tm">Sign this petition</a></p>
+    <p><i class="bi bi-megaphone-fill"></i>&nbsp;<a href="https://javascript.tm/">Tell Oracle to free Javascript</a></p>
   </div>
 </template>
 <style scoped>
