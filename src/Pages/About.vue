32c82aa34f9d4b2183efc7ca1b8b076a9be0088e
吳元皓 <157942818+hpware@users.noreply.github.com>
2024-09-19 09:18:49 +0800

    Add stuff

--- src/Pages/About.vue
+++ src/Pages/About.vue
@@ -4,9 +4,10 @@
   <h2 class="pagetitle">關於我</h2>
   <section id="aboutme">
     <h3>自我介紹</h3>
-    <p>我是一個學生，準備在臺灣就讀有關資訊工程的五專。</p>
+    <p>我是一個學生，在臺灣就讀有關資訊工程的五專。</p>
     <p>熟悉 HTML, CSS , Vercel 與 Git(Hub) </p>
     <p>我喜歡把 Prod 當測試環境。</p>
+<!--Add web-->
     <p>小時候是我就喜歡用電腦，但我當時一點程式都不會寫。<br>
       我2020年時剛開始想要並使用自己的Wordpress的部落格 <a href="https://hwweb.i234.me/wordpress">hwweb.i234.me/wordpress</a>，
       <br>但Synology 不讓我把Wordpress放在 Website Root 裡面，所以我決定要改預設的檔案來讓使用者到部落格。
@@ -15,10 +16,15 @@
       <br>當時的我沒有放入< a > 的標籤，所以不是 HyperLink，我當時也沒想太多就繼續使用著。
       <br>但我在這一年內增加了許多HTML & CSS的知識，讓我更有信心可以做的更好！
     </p>
+<!--Pythib-->
     <p>
       我也在國中一年級時開始學習Python，這個也是我想學的語言，但我教學影片只看但10％的地方就放棄了。
       <br>我也之後大多數都忘記了，連 if else 每次都要查一下，因為我比較習慣用 JavaScript 的方式 if {} else if {} else {}。
     </p>
+<!--IT
+	<p>我在2019年時，發現2018年買的NAS可以放虛擬機器，剛開始只是跑 Windows 7 而已，
+		但一段時間後我發現Linux，我也開始學一點Debain Cli (Ubuntu)
+	</p>-->
   </section>
   <br>
   <hr>
