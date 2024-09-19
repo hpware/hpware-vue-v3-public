4fdf1f728b4418a9242f4bfeb38eeb198b229e05
Howard <hw@hwtw.cc>
2024-09-19 23:52:13 +0800

    Small change.

--- src/Pages/Other.vue
+++ src/Pages/Other.vue
@@ -5,8 +5,8 @@
   <h2>其他</h2>
   <p>
     <a href="/InfectedSites">已中毒網站與網頁</a><br>
-    <a href="/Admin/index">Admin Center</a><br>
-    <a href="https://git.hwtw.cc">Git 伺服器 2</a><br>
+    <a href="/Admin">管理員系統 (Beta)</a><br>
+    <a href="https://git.hwtw.cc">Git 伺服器</a><br>
     <a href="https://zz.yuanhau.com">檔案分享伺服器</a><br>
       <a href="https://vd.yuanhau.com">使用者調查系統</a><br>
   </p>
