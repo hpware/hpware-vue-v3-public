89ca10eae48b0a99415ab1de3feff8c8954b06fa
Howard <hw@hwtw.cc>
2024-09-22 00:07:10 +0800

    Fixing plaintext passwords in js files

--- src/Pages/Admin.vue
+++ src/Pages/Admin.vue
@@ -1,10 +1,13 @@
 <script setup>
 // Default
+'use server';
 import { ref } from 'vue';
 import NavSpace from '@/components/Other/NavSpace.vue';
+require('dotenv').config();
 // Login Function
 const login = ref('no');
 const username = ref('');
+const logout = ref('');
 const ENVusername = import.meta.env.VITE_ADMINUSERNAME;
 const pwd = ref('');
 const ENVpwd = import.meta.env.VITE_ADMINPWD;
@@ -33,12 +36,24 @@ const toggleBlogCreate = () => {
   }
   BlogCreateTransition.value = !BlogCreateTransition.value;
 }
+// Logout & Change Password (Nav Bar for Admins)
+const logoutAction = () => {
+  login.value = '';
+  logout.value = 'yes';
+}
+const changePasswordAction = () => {
+  alert('密碼更改功能暫時未開放使用');
+}
 </script>
 
 <template>
   <div class="admin-page">
+    <h3>管理員系統</h3>
+    <h4 v-if="logout === 'yes'"><i class="bi bi-box-arrow-left"></i>&nbsp;登出成功!</h4>
     <div v-if="login === 'yes'">
-      <h3>管理員系統 (Beta)</h3>
+      <button @click="logoutAction();" class="not-a-button"><i class="bi bi-box-arrow-left"></i>登出</button><button @click="changePasswordAction" class="not-a-button"><i class="bi bi-option"></i> 更改密碼</button><br>
+      <i class="bi bi-person-circle" style="font-size: 3em; color: #5ad9ef;"></i><br>
+      <h4>Actions:</h4>
       <button @click="toggleAnnouncements();">更改公告</button>
       <NavSpace />
       <button @click="toggleBlogCreate();">新增文章</button>
@@ -56,20 +71,20 @@ const toggleBlogCreate = () => {
 
     </div>
     <div v-if="login === 'no'">
-      <h3>管理員登入系統 (Beta)</h3>
       <p>這個網頁必須登入才可使用</p><br>
       <form @submit="checkUser">
 				<label for="username">使用者</label><br>
 				<input type="user" v-model="username" required><br>
-        <label for="password">密碼</label><br>
+        <label for="password">&nbsp;密碼</label><br>
         <input type="password" v-model="pwd" required>
         <br>
         <button type="submit">送出</button>
       </form>
     </div>
     <div v-if="login === 'failed'">
-      <h3>管理員登入系統 (Beta)</h3>
-      <p>登入失敗☹️</p>
+      <i class="bi bi-person-circle" style="font-size: 3em; color: red;"></i>
+      <p>登入失敗 ( •̯́ ^ •̯̀)</p><br>
+      <button onclick="window.location.href=window.location.href" style="background-color:khaki">重試</button>
     </div>
   </div>
 </template>
@@ -87,4 +102,12 @@ input, textarea {
 textarea {
   font-family: Arial, Helvetica, sans-serif;
 }
+  .not-a-button {
+    background-color: transparent;
+    border: none;
+    color: rgba(191, 191, 191, 0.7);
+    font-size: 1.0em;
+    cursor: pointer;
+    border-radius: 50%;
+  }
 </style>
