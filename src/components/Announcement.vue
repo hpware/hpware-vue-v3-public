0b2234226e598cee5705ebcba400beeb78ca4f79
hpware-bot1 <bot+announcements@yuanhau.com>
2024-09-24 23:37:27 +0800

    公告關閉

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -4,9 +4,9 @@
 
 <template>
   <div class="a">
-    <p><i class="bi bi-megaphone-fill"></i>&nbsp;現在網站安全性已提高，但還是有些功能還是用PlainText Password ，這周會完成Migration。</p>
   </div>
 </template>
+
 <style scoped>
 
 .a {
