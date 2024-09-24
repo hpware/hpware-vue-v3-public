1eac6e1cb8715fc20088f96fd5477c8592333012
Howard <hw@hwtw.cc>
2024-09-24 21:11:43 +0800

    Change config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { unheadComposablesImports } from 'unhead'
+import AutoImport from 'unplugin-auto-import/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,6 +14,11 @@ export default defineConfig({
         org: "hwtwcc",
         project: "hwtwcc-vue"
       }),
+      AutoImport({
+          imports: [
+              unheadComposablesImports[0],
+          ]
+      })
   ],
 
   resolve: {
