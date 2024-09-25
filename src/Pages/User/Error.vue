0451e93386e8a0ed55ada92e332e0115c5488463
E1301 <s51331102@tpcu.edu.tw>
2024-09-25 11:15:13 +0800

    Ok great! It works now :)

---
+++ src/Pages/User/Error.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <i class="bi bi-person-circle" style="font-size: 3em; color: red"></i>
+    <p>密碼錯誤或使用者不存在 ( •̯́ ^ •̯̀)</p>
+    <br />
+    <button
+        onclick="window.location.href=window.location.href"
+        style="background-color: khaki"
+    >
+      重試
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
