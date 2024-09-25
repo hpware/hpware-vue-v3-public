2adce687e1456a5439740377892536e72f699bca
Howard <hw@hwtw.cc>
2024-09-25 16:25:35 +0800

    Uncomplete features

---
+++ src/Fixing.vue
@@ -0,0 +1,30 @@
+<template>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>網站維護中</title>
+      <div class="f">
+        <span>網站維護中</span><br>
+        <span class="content2">請稍後再試，如造成您的不便，敬請見諒。</span>
+  </div>
+</template>
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  background-color: #232730;
+  color: white;
+  justify-content: center;
+}
+.f {
+  margin-top: 60px;
+  padding: 20px;
+  display: flex;
+  flex: 0 0 50%;
+  box-sizing: border-box;
+  flex-direction: column;
+  justify-content: center;
+  min-width: 380px;
+}
+</style>
+<script setup lang="ts">
+</script>
--- src/Pages/User/Area/BusTrack.vue
+++ src/Pages/User/Area/BusTrack.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<script setup>
+import NavSpace from "@/components/Other/NavSpace.vue";
+import cookie from "vue-cookie";
+import CryptoJS from "crypto-js";
+import AES from "crypto-js/aes";
+import SHA256 from "crypto-js/sha256";
+if (cookie.get("user")) {
+}
+</script>
 
-<template></template>
+<template>
+  <div>
+    <p>此功能未完成 ( •̯́ ^ •̯̀)</p>
+    <br />
+
+</template>
 
 <style scoped></style>
