6b4216b411ae5d83a9a6cbb0b05da1ebb402830e
Howard <hw@hwtw.cc>
2024-09-27 21:16:31 +0800

    Change Links

--- src/components/Other/NavSpace.vue
+++ src/components/Other/NavSpace.vue
@@ -1,7 +1,30 @@
-<script setup></script>
+<script setup>
+</script>
 
-<template><span class="desktop">&nbsp;</span><br class="mobile" /></template>
+<template>
+  <br class="mobile" />
+  &nbsp;
+</template>
 
 <style scoped>
-
+br.mobile {
+  display: none;
+}
+span.mobile {
+  display: none;
+}
+@media (max-width: 502px) {
+  br.mobile {
+    display: block;
+  }
+  br.desktop {
+    display: none;
+  }
+  span.mobile {
+    display: block;
+  }
+  span.desktop {
+    display: none;
+  }
+}
 </style>
