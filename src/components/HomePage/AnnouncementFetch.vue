4d9bac4f377a5770d8943dc1ff3fb554d9e908cf
Howard <hw@hwtw.cc>
2024-09-29 22:44:33 +0800

    Add New Beta Fetcher

---
+++ src/components/HomePage/AnnouncementFetch.vue
@@ -0,0 +1,16 @@
+<script setup>
+// Setup
+import { onMounted, ref } from "vue";
+onMounted (async () => {
+  const source = await fetch("https://api.yuanh.xyz/api/announcements");
+  const data = await source.json();
+});
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
