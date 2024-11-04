fc5914d6363be77c23b6c8cb5d4600eeafa434f8
Howard <hw@yuanhau.com>
2024-11-04 22:34:32 +0800

    Refactor BlogIndex component to fetch blogs from API instead of local filesystem

--- src/components/BlogingSystem/BlogIndex.vue
+++ src/components/BlogingSystem/BlogIndex.vue
@@ -1,57 +1,51 @@
 <script setup>
-    import { ref, onMounted } from "vue";
-    import fs from "fs";
-    import path from "path";
-    
-    const blogs = ref([]);
-    const error = ref(false);
-    const errorText = ref("");
-    const loading = ref(false);
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
-    onMounted(() => {
-        getBlogs();
-    });
-    
+const blogs = ref([]);
+const error = ref(false);
+const errorText = ref("");
+const loading = ref(false);
+
+onMounted(() => {
+    getBlogs();
+});
+
 async function getBlogs() {
     loading.value = true;
-        const dirPath = path.join(__dirname, "@/BlogPosts/");
-        fs.readdir(dirPath, (err, files) => {
-            console.log("Reading files from directory...");
-            if (err) {
-                error.value = true;
-                errorText.value = "讀取文章時發生錯誤。";
-                return;
-            } 
-            blogs.value = files.filter((file) => file.endsWith(".md"));
-            console.log("Files received:", files);
-            console.log("Filtered blogs:", blogs.value);
-        });
+    try {
+        const response = await axios.get('/path/to/your/blogs/api');
+        blogs.value = response.data;
+    } catch (err) {
+        error.value = true;
+        errorText.value = "讀取文章時發生錯誤。";
+    } finally {
         loading.value = false;
+    }
 }
 </script>
 
 <template>
     <div>
         <div class="loading" v-if="loading">
-            <!--Loading Gif Placement Here.-->
+            <!--Loading GIF Here-->
             <h3>Loading...</h3>
         </div>
         <div v-if="!loading">
-        <h3 class="error" v-if="error">{{ errorText }}</h3>
-        <div class="item" v-for="blog in blogs" :key="blog">
-            <a :href="`/blog/${blog}`">
-                <h4>{{ blog }}</h4>
-            </a>
+            <h3 class="error" v-if="error">{{ errorText }}</h3>
+            <div class="item" v-for="blog in blogs" :key="blog">
+                <a :href="`/blog/${blog}`">
+                    <h4>{{ blog }}</h4>
+                </a>
+            </div>
         </div>
-    </div>
     </div>
 </template>
 
 <style scoped>
-    h3.error {
-        color: red;
-    }
-    div.item {
-
-    }
+h3.error {
+    color: red;
+}
+div.item {
+}
 </style>
